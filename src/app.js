8cb76f88d5d7bc05df2026eea36a1bf12033305b
Manoj170505 <manojfa4451e@gmail.com>
2026-02-04 22:17:40 +0530

    Update CORS to allow all origins for production

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: '*', // Allow all origins for production
     credentials: true
 }));
 
